1015babcfcd929c65982df8b132eec2874963a6a
thehalen <halen@alumni.chalmers.se>
2021-10-11 13:45:48 +0200

    Add project files.

---
+++ LexiconA4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LexiconA4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
